10f5171e1d9e047831dbcb8a16b3c3d9e17e818b
Sasasu <su@sasasu.me>
2020-11-30 18:08:39 +0800

    WIP: add fish auto complete

--- fly/commands/completion.go
+++ fly/commands/completion.go
@@ -1,9 +1,12 @@
 package commands
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 type CompletionCommand struct {
-	Shell string `long:"shell" required:"true" choice:"bash" choice:"zsh"` // add more choices later
+	Shell string `long:"shell" required:"true" choice:"bash" choice:"zsh" choice:"fish"` // add more choices later
 }
 
 // credits:
@@ -18,6 +21,46 @@ const bashCompletionSnippet = `_fly_compl() {
 complete -F _fly_compl fly
 `
 
+func fishCompletionSnippetHelper(snippet string, prefix string, commandType reflect.Type) string {
+	for i := 0; i < commandType.NumField(); i++ {
+		var tags = commandType.Field(i).Tag
+
+		var command = tags.Get("command")
+		var long, short = tags.Get("long"), tags.Get("short")
+		var description = tags.Get("description")
+
+		var template = "complete --no-files -c fly"
+
+		if prefix != "" {
+			template += fmt.Sprintf(" -n '__fish_seen_subcommand_from %s'", prefix)
+		}
+
+		if command != "" {
+			snippet += fishCompletionSnippetHelper(template, prefix+" "+command, commandType.Field(i).Type)
+
+			template += fmt.Sprintf(" -n '__fish_use_subcommand' -a %s", command)
+		}
+
+		if description != "" {
+			template += fmt.Sprintf(` -d "%s"`, description)
+		}
+
+		if long != "" {
+			template += fmt.Sprintf(" --l %s", long)
+		}
+
+		if short != "" {
+			template += fmt.Sprintf(" -s %s", short)
+		}
+
+		snippet += template + "\n"
+	}
+
+	return snippet
+}
+
+var fishCompletionSnippet = fishCompletionSnippetHelper("", "", reflect.TypeOf(Fly))
+
 // initial implemenation just using bashcompinit
 const zshCompletionSnippet = `autoload -Uz compinit && compinit
 autoload -Uz bashcompinit && bashcompinit
@@ -30,6 +73,9 @@ func (command *CompletionCommand) Execute([]string) error {
 		return err
 	case "zsh":
 		_, err := fmt.Print(zshCompletionSnippet)
+		return err
+	case "fish":
+		_, err := fmt.Print(fishCompletionSnippet)
 		return err
 	default:
 		// this should be unreachable
